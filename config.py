ddf1d9947131e118d13e20b5882cca877ce06c21
azik-com <azimbekusmonov.007@gmail.com>
2026-03-16 12:19:00 +0500

    Config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 # =============================================
 
 # 👇 O'z tokeningizni shu yerga qo'ying
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8614126964:AAGJTbSjM0y73dLmY8At6gRVHWgYyZqjrfg"
 
 # O'yin sozlamalari (ixtiyoriy o'zgartirish mumkin)
 NIGHT_TIMEOUT = 45   # Kecha vaqti (soniya)
 DAY_TIMEOUT = 90     # Kunduz ovoz berish vaqti (soniya)
 MIN_PLAYERS = 4      # Minimal o'yinchi soni
-MAX_PLAYERS = 15     # Maksimal o'yinchi soni
+MAX_PLAYERS = 40     # Maksimal o'yinchi soni
